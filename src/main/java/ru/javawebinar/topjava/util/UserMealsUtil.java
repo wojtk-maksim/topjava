1caed09fd2e88266fcb4ef12dddb715071cea148
Maksim Wojtk <wojtk.maksim@gmail.com>
2023-07-28 14:34:46 +0300

    Optional filter by custom collector method added

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -8,7 +8,15 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.*;
-import java.util.stream.Collectors;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -26,6 +34,8 @@ public class UserMealsUtil {
         mealsTo.forEach(System.out::println);
 
         System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+
+        System.out.println(filteredByCustomCollector(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
     // Old style Java
@@ -50,7 +60,7 @@ public class UserMealsUtil {
             }
         }
 
-        mealsWithExcess.sort(new Comparator<UserMealWithExcess>() {
+        mealsWithExcess.sort(new Comparator<>() {
             @Override
             public int compare(UserMealWithExcess o1, UserMealWithExcess o2) {
                 return o2.getDateTime().compareTo(o1.getDateTime());
@@ -63,16 +73,108 @@ public class UserMealsUtil {
     // New style Java
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         Map<LocalDate, Integer> caloriesByDate = meals.stream()
-                .collect(Collectors.toMap(UserMeal::getDate, UserMeal::getCalories, Integer::sum));
+                .collect(toMap(UserMeal::getDate, UserMeal::getCalories, Integer::sum));
 
         return meals.stream()
                 .filter(m -> TimeUtil.isBetweenHalfOpen(m.getTime(), startTime, endTime))
                 .map(m -> mealWithExcess(m, caloriesByDate.get(m.getDate()) > caloriesPerDay))
-                .sorted(Comparator.comparing(UserMealWithExcess::getDateTime).reversed())
-                .collect(Collectors.toList());
+                .sorted(comparing(UserMealWithExcess::getDateTime).reversed())
+                .collect(toList());
     }
 
     private static UserMealWithExcess mealWithExcess(UserMeal meal, boolean excess) {
         return new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), excess);
+    }
+
+    // Optional filter by custom collector
+    public static List<UserMealWithExcess> filteredByCustomCollector(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        // Helper class for storing calories and meals. Can be mapped to specific date
+        class DateData {
+            private int calories;
+
+            private final List<UserMeal> meals;
+
+            public DateData(int calories, List<UserMeal> meals) {
+                this.calories = calories;
+                this.meals = meals;
+            }
+
+            public void addCalories(int calories) {
+                this.calories += calories;
+            }
+
+            public void addMeal(UserMeal meal) {
+                meals.add(meal);
+            }
+
+            public void addMeals(List<UserMeal> meals) {
+                this.meals.addAll(meals);
+            }
+        }
+
+        return meals.stream().collect(
+                // Accumulator is a HashMap<LocalDate, DateData>. DateData keeps meals in LinkedList
+                new Collector<UserMeal, Map<LocalDate, DateData>, List<UserMealWithExcess>>() {
+                    @Override
+                    public Supplier<Map<LocalDate, DateData>> supplier() {
+                        return HashMap::new;
+                    }
+
+                    @Override
+                    public BiConsumer<Map<LocalDate, DateData>, UserMeal> accumulator() {
+                        return (map, meal) -> map.compute(
+                                meal.getDate(),
+                                (date, dateData) -> {
+                                    if (dateData == null) {
+                                        dateData = new DateData(meal.getCalories(), new LinkedList<>());
+                                    } else {
+                                        dateData.addCalories(meal.getCalories());
+                                    }
+
+                                    if (TimeUtil.isBetweenHalfOpen(meal.getTime(), startTime, endTime)) {
+                                        dateData.addMeal(meal);
+                                    }
+                                    return dateData;
+                                });
+                    }
+
+                    @Override
+                    public BinaryOperator<Map<LocalDate, DateData>> combiner() {
+                        return (leftMap, rightMap) -> {
+                            rightMap.forEach((date, dateData) -> leftMap.merge(
+                                    date,
+                                    dateData,
+                                    (leftDateData, rightDateData) -> {
+                                        leftDateData.addCalories(rightDateData.calories);
+                                        leftDateData.addMeals(rightDateData.meals);
+                                        return leftDateData;
+                                    }
+                            ));
+                            return leftMap;
+                        };
+                    }
+
+                    @Override
+                    public Function<Map<LocalDate, DateData>, List<UserMealWithExcess>> finisher() {
+                        return (map) -> {
+                            List<UserMealWithExcess> mealsWithExcess = new ArrayList<>();
+                            map.forEach((date, dateData) -> {
+                                boolean excess = dateData.calories > caloriesPerDay;
+                                dateData.meals.forEach(
+                                        m -> mealsWithExcess.add(mealWithExcess(m, excess))
+                                );
+                            });
+
+                            mealsWithExcess.sort(comparing(UserMealWithExcess::getDateTime).reversed());
+                            return mealsWithExcess;
+                        };
+                    }
+
+                    @Override
+                    public Set<Collector.Characteristics> characteristics() {
+                        return EnumSet.of(Collector.Characteristics.UNORDERED);
+                    }
+                }
+        );
     }
 }

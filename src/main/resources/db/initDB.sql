79fbc5c2e870061ed21738c7830af404227971c8
Maksim Wojtk <wojtk.maksim@gmail.com>
2023-08-04 16:40:52 +0300

    Add meals table and JDBC meal repository

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_role;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
@@ -22,4 +23,15 @@ CREATE TABLE user_role
     role    VARCHAR NOT NULL,
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+    id              INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    user_id         INTEGER                             NOT NULL,
+    date_time       TIMESTAMP                           NOT NULL,
+    description     VARCHAR                             NOT NULL,
+    calories        INTEGER                             NOT NULL,
+    CONSTRAINT user_unique_date_time UNIQUE (user_id, date_time),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,9 @@
-DELETE FROM user_role;
-DELETE FROM users;
+DELETE
+FROM meals;
+DELETE
+FROM user_role;
+DELETE
+FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -10,3 +14,15 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_role (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories)
+VALUES (100000, '2020-01-30 10:00', 'Завтрак', 500),
+       (100000, '2020-01-30 13:00', 'Обед', 1000),
+       (100000, '2020-01-30 20:00', 'Ужин', 500),
+       (100000, '2020-01-31 00:00', 'Еда на граничное значение', 100),
+       (100000, '2020-01-31 10:00', 'Завтрак', 1000),
+       (100000, '2020-01-31 13:00', 'Обед', 500),
+       (100000, '2020-01-31 20:00', 'Ужин', 410),
+       (100001, '2020-01-31 13:00', 'Админ обед', 700),
+       (100001, '2020-01-31 18:00', 'Админ ужин', 800);
+
